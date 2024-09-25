a09d96460b6d795f8c6abc0bdcfa231676048281
ItsHLa <nissanihala@gmail.com>
2024-09-25 17:35:08 +0300

    added clean up package for images

--- tweety_api/settings.py
+++ tweety_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from datetime import timedelta
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,6 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
 
--- tweety_api/urls.py
+++ tweety_api/urls.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweety/auth/' , include('accounts.urls')),
     path('tweety/posts/' , include('tweety_posts.urls')),
     path('tweety/comments/' , include('tweety_comments.urls')),
     path('tweety/profile/' , include('user_profile.urls')),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
